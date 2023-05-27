db407d435d9180b864b465611e3c63be22be86aa
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-05-27 16:05:33 +0800

    garnet: Import idc & keylayout from stock
    * from V14.0.6.0.TNRMIXM
    
    Signed-off-by: ramaadni <ramadhanifals@gmail.com>
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- device.mk
+++ device.mk
@@ -259,6 +259,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/configs/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-goodix.idc \
+    $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
