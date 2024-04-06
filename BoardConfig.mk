174bed42ea066f5d2183cf5a8de8e248e906f522
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-04-07 00:14:59 +0530

    garnet: Add support for Dolby Atmos
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,6 +70,7 @@ BOARD_USES_QCOM_HARDWARE := true
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/derp/config/device_framework_matrix.xml
