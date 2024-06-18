d36da2161ffad7af1603bc69f7dd6c3ece662178
UnmoveD <unmoved2121@gmail.com>
2024-06-18 21:57:51 +0200

    garnet: Import xiaomi_framework_compatibility_matrix.xml
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,8 +70,8 @@ BOARD_USES_QCOM_HARDWARE := true
 # HIDL
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/derp/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
