5b944f4c9f50865a810f77953c4edcfa658958ff
Joey <joey@evolution-x.org>
2024-06-21 02:45:19 +0900

    garnet: Declare more media packages for Dolby
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- device.mk
+++ device.mk
@@ -337,8 +337,13 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.2.vendor \
     libOmxCore \
+    libavservices_minijail.vendor \
+    libavservices_minijail_vendor \
+    libcodec2_hidl@1.2.vendor \
     libcodec2_hidl@1.0.vendor \
+    libcodec2_soft_common.vendor \
     libcodec2_vndk.vendor \
     libstagefrighthw \
     libstagefright_softomx_plugin.vendor
