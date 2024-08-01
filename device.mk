9fdd2a9086e4f4b56966448049417ec701975218
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-01 13:57:26 +0530

    garnet: Override HDR types for HDR
    on stock this device used to advertise hdr capabilities with miui fw and
    blobs but this changed with hyperos they only report dolby vision now,
    lets override HdrCapabilities till we find a proper way to bring it back
    
    Change-Id: I93ecfbd7aef42e5e1a4c696e196e482a023c9ae4

--- device.mk
+++ device.mk
@@ -300,6 +300,10 @@ PRODUCT_PACKAGES += \
     vendor_firmware_mnt_mountpoint \
     vendor_vm-system_mountpoint
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
