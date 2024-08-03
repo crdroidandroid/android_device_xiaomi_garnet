f9eeb2cb6d2c34f0b00e8649e7c557ba0077a180
Karan Parashar <karan@pixelos.net>
2024-08-03 22:30:04 +0530

    garnet: Build libutils.vendor
    E vndksupport: Could not load libutils.so from sphal namespace: dlopen failed: library "libutils.so" not found.
    
    Change-Id: I5bd9623fa7a25efd5c429bac3eca78e10855219e
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- device.mk
+++ device.mk
@@ -156,6 +156,9 @@ PRODUCT_PACKAGES += \
     libyuv.vendor \
     libcamera2ndk_vendor
 
+PRODUCT_PACKAGES += \
+    libutils.vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
