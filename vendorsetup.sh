a5c89b39d670693d3291c53525b32cdf1e41e78f
UnmoveD <unmoved2121@gmail.com>
2024-10-02 19:39:03 +0200

    garnet: update vendorsetup.sh
    Change-Id: I4ebe7c5164ec860c101740eddd77d35974026104

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,15 +2,15 @@ echo 'Starting to clone stuffs needed to build for garnet'
 
 # Vendor
 echo 'Cloning vendor tree'
-git clone https://github.com/aosp-garnet/android_vendor_xiaomi_garnet.git -b derp-14 vendor/xiaomi/garnet
+git clone https://github.com/aosp-garnet/android_vendor_xiaomi_garnet.git -b 15 vendor/xiaomi/garnet
 
 # Kernel
 echo 'Cloning kernel tree'
-git clone https://github.com/aosp-garnet/android_device_xiaomi_garnet-kernel.git -b lineage-21 device/xiaomi/garnet-kernel
+git clone https://github.com/aosp-garnet/android_device_xiaomi_garnet-kernel.git -b 15 device/xiaomi/garnet-kernel
 
 # Hardware Xiaomi
 echo 'Cloning hardware xiaomi'
-rm -rf hardware/xiaomi && git clone https://github.com/aosp-garnet/hardware_xiaomi.git -b lineage-21 hardware/xiaomi
+rm -rf hardware/xiaomi && git clone https://github.com/aosp-garnet/hardware_xiaomi.git -b 15 hardware/xiaomi
 
 # MiuiCamera
 echo 'Cloning miuicamera tree'
@@ -18,6 +18,6 @@ git clone https://gitlab.com/unmoved21/vendor_xiaomi_garnet-miuicamera -b 14 ven
 
 # Display Hals
 echo 'Cloning display hals'
-rm -rf hardware/qcom-caf/sm8450/display && git clone https://github.com/aosp-garnet/hardware_qcom-caf_sm8450_display.git -b lineage-21 hardware/qcom-caf/sm8450/display
+rm -rf hardware/qcom-caf/sm8450/display && git clone https://github.com/aosp-garnet/hardware_qcom-caf_sm8450_display.git -b 15 hardware/qcom-caf/sm8450/display
 
 echo 'Cloning process is completed, now its time for lunch'
