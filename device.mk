8c9f5de5ce8519c1f8d1821dc28e52efef4fbc8d
kleidione Freitas <kleidione@gmail.com>
2024-11-02 21:35:12 -0300

    garnet: Adapted to OP Dolby configuration Using dolby from: https://github.com/JYRRC/hardware_dolby
    Signed-off-by: kleidione <kleidione@gmail.com>
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Co-authored-by: userariii <abhattacharjee717@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -188,8 +188,11 @@ PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8450/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
 # Dolby
-PRODUCT_PACKAGES += \
-    XiaomiDolby
+#PRODUCT_PACKAGES += \
+    #XiaomiDolby
+
+# Dolby Oneplus
+$(call inherit-product, hardware/dolby/dolby.mk)
 
 # Shims
 PRODUCT_PACKAGES += \
@@ -350,6 +353,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_parrot_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_parrot_v0.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
