3758f15d5a4525f2ad6bda323619e9ac29f60c4f
kenway214 <kenway214@outlook.com>
2024-11-28 16:43:22 +0530

    garnet: parts: Start parts services as early as possible on bootup
    BOOT_COMPLETED intent is broadcast only after the device is unlocked and user data (encrypted storage) is available.
    Use LOCKED_BOOT_COMPLETED intent for services that don't require accessing data.
    
    Change-Id: I800df08f9c61925ac394f3e6d4861ba68a157797
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Co-Authored-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -36,11 +36,21 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG)
-            Log.d(TAG, "Received boot completed intent");
-        ThermalUtils.startService(context);
-        RefreshUtils.startService(context);
-        overrideHdrTypes(context);
+        if (DEBUG) Log.i(TAG, "Received intent: " + intent.getAction());
+        switch (intent.getAction()) {
+            case Intent.ACTION_LOCKED_BOOT_COMPLETED:
+                onLockedBootCompleted(context);
+                break;
+            case Intent.ACTION_BOOT_COMPLETED:
+                onBootCompleted(context);
+                break;
+        }
+    }
+
+    private static void onLockedBootCompleted(Context context) {
+            ThermalUtils.startService(context);
+            RefreshUtils.startService(context);
+            overrideHdrTypes(context);
     }
 
     private static void overrideHdrTypes(Context context) {
@@ -49,5 +59,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         dm.overrideHdrTypes(Display.DEFAULT_DISPLAY, new int[]{
                 HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
                 HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
+    }
+
+    private static void onBootCompleted(Context context) {
     }
 }
