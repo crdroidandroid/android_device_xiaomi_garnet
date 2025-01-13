1b84e107b21e14aaf61b9511604bc93c2a73994f
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    garnet: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -408,6 +408,13 @@ PRODUCT_PACKAGES += \
     WifiOverlayGarnetRedmiCN \
     WifiOverlayGarnetXIG05
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint \
+    vendor_vm-system_mountpoint
+
 # Parts
 PRODUCT_PACKAGES += \
     XiaomiParts
