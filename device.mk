2506a2d7db25d3f059d09b4939c8ee3ecd63f725
Michael Bestas <mkbestas@lineageos.org>
2025-01-15 14:54:58 -0300

    garnet: Switch to AIDL thermal HAL
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120

--- device.mk
+++ device.mk
@@ -506,7 +506,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti-v2 \
+    android.hardware.thermal-service.qti \
     android.hardware.thermal@2.0.vendor
 
 # Touchscreen
