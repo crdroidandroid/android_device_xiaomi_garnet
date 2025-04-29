7109fad6b4a87e7616f75a4a7de0695a2e8d9eb3
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:21:39 +0100

    garnet: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- device.mk
+++ device.mk
@@ -266,6 +266,8 @@ PRODUCT_PACKAGES += \
     IFAAService
 
 # Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_garnet)
+
 PRODUCT_PACKAGES += \
     charger_fw_fstab.qti:64 \
     fstab.qcom:64 \
