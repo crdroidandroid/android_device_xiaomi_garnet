9886d80bcbf6d507c7cf1cd903721f6d6b71a872
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    garnet: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- device.mk
+++ device.mk
@@ -325,6 +325,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor
