39560cabe4c85b54d4da12fcc9fbd0641139a80c
dianlujitao <dianlujitao@lineageos.org>
2025-07-25 20:15:59 +0800

    garnet: Set force_sysfs_fallback=1 sysctl via cmdline
    early-init class gets triggered after all recovery modules are already
    loaded, which prevents ADSP firmware from being loaded.
    
    Change-Id: I4eaf2890bd98a6a11b6a87ad7330827fbf9b44d7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,7 @@ BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
+    sysctl.kernel.firmware_config.force_sysfs_fallback=1 \
     bootinfo.fingerprint=$(LINEAGE_VERSION) \
     swinfo.fingerprint=$(LINEAGE_VERSION)
 
