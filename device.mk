e422bacbbfa6638bf0127c7f241151e45c4ecda0
MySelly <xusufgul@gmail.com>
2025-07-29 16:15:50 +0300

    garnet: Nuke libinit_garnet

--- device.mk
+++ device.mk
@@ -218,8 +218,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
-$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_garnet)
-
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
