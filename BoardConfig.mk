3e18bd02ca089b2e9053fe5c0a1d97072b7de3a8
MySelly <xusufgul@gmail.com>
2025-10-11 19:44:26 +0000

    Revert "garnet: Include forgotten product.prop"
    This reverts commit 7faebd80a4e1b363f26fab204116371b8c959702.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,7 +212,6 @@ TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/props/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/props/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/props/vendor.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/props/product.prop
 
 # Vendor security patch
 VENDOR_SECURITY_PATCH := 2025-08-01
