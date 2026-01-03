96e3ad513f5acdffd690416f4cfe1018b663abb1
MySelly <xusufgul@gmail.com>
2026-01-03 17:29:55 +0000

    garnet: Change kernel location

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,7 +106,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := TARGET_PRODUCT=garnet
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm7435
+TARGET_KERNEL_SOURCE := kernel/xiaomi/garnet
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/parrot_GKI.config \
@@ -130,7 +130,7 @@ BOARD_BOOTCONFIG := \
     androidboot.usbcontroller=a600000.dwc3
 
 # Kernel modules
-TARGET_KERNEL_EXT_MODULE_ROOT := kernel/xiaomi/sm7435-modules
+TARGET_KERNEL_EXT_MODULE_ROOT := kernel/xiaomi/garnet-modules
 TARGET_KERNEL_EXT_MODULES := \
 	qcom/opensource/mmrm-driver \
 	qcom/opensource/audio-kernel \
