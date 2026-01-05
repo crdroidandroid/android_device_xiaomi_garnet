acf1a256c73ceaa67966b89de976713fdaecb7ef
MySelly <xusufgul@gmail.com>
2026-01-05 09:38:34 +0000

    garnet: Change miuicamera location

--- device.mk
+++ device.mk
@@ -476,7 +476,7 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
 
 # Vendor MiuiCamera
-$(call inherit-product-if-exists, device/xiaomi/garnet-miuicamera/device.mk)
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-garnet/device.mk)
 
 # Sign Keys
 -include vendor/lineage-priv/keys/keys.mk
