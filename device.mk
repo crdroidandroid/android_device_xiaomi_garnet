43b991b9d9c56a15a4fc9e887024cea08839337d
Jotanh <jotanh@jotanic.xyz>
2026-06-23 04:55:13 +0000

    Revert "garnet: Implement torch light control"
    This reverts commit 19e93822e245c9ba66ad2bc1dfd578202c8afb1b.

--- device.mk
+++ device.mk
@@ -127,9 +127,6 @@ $(call soong_config_set, ufsbsg, ufsframework, bsg)
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor
 
-$(call soong_config_set_bool,camera,override_format_from_reserved,true)
-$(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.xiaomi_sm7435)
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
