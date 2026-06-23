83fdbe2f5004e8e110143296c367317c7fbca451
Jotanh <jotanh@jotanic.xyz>
2026-06-23 04:55:10 +0000

    Revert "garnet: Fix Torch Integration"
    This reverts commit 76baf507a1b016f51967e229f0056ffeee1f5ee0.

--- device.mk
+++ device.mk
@@ -128,7 +128,7 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor
 
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
-$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.xiaomi_sm7435)
+$(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.xiaomi_sm7435)
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
